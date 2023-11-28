8dcb0f676b5ee0e9e4d9409288d804fe8aa77a23
prasadnaidu <prasadnaidu.marolix@gmail.com>
2023-11-28 11:18:31 +0530

    SHKPdeliveryappfunctionalities application generic django template

---
+++ SHKPdeliveryappfunctionalities/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ShkpdeliveryappfunctionalitiesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'SHKPdeliveryappfunctionalities'
